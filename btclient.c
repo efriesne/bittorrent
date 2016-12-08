dbb0f02da489773031db0d671c53c1431274cd95
efriesne <emma_friesner@brown.edu>
2016-12-08 17:50:38 -0500

    tracker request and response functions

--- btclient.c
+++ btclient.c
@@ -33,18 +33,76 @@ char *read_file(const char *file, long long *len) {
 	return ret;
 }
 
+size_t tracker_response_func(void *ptr, size_t size, size_t nmemb, void *stream) {
+	size_t len;
+	char *response_str = NULL;
+	be_node *n;
+
+	len = size * nmemb;
+	response_str = (char *)malloc(len);
+	if (!response_str)
+		return -1;
+
+	fread(response_str, 1, len, (FILE *)stream);
+	fclose(stream);
+
+	n = be_decoden(response_str, len);
+	if (n) {
+		be_dump(n);
+		be_free(n);
+	} else {
+		printf("\tparsing tracker response failed!\n");
+	}
+	free(response_str);
+}
+
+add_url_param(char *url, char *key, char* val, int end) {
+	strcat(url, key);
+	strcat(url, "=");
+	strcat(url, val);
+	if (!end) {
+		strcat(url, "&");
+	}
+}
+
+tracker_request_func(torrent_ctrl_t *tc, char *event) {
+	CURL *curl;
+	CURLcode res;
+	char *url;
+	FILE *tracker_response_file;
+	char url_buf[MAX_URL_SIZE];
+	memset(url_buf, 0, MAX_URL_SIZE);
+
+	curl_global_init(CURL_GLOBAL_DEFAULT); 
+	curl = curl_easy_init();
+	if (curl) {
+		strcat(url_buf, tc.tracker_url);
+		strcat(url_buf, "?");
+		add_url_param(url_buf, "info_hash", tc.info_hash, 0);
+		add_url_param(url_buf, "peer_id", client_id, 0);
+		add_url_param(url_buf, "port", port, 0);
+		add_url_param(url_buf, "uploaded", tc.uploaded, 0);
+		add_url_param(url_buf, "downloaded", tc.downloaded, 0);
+		add_url_param(url_buf, "left", tc.torrend_len - downloaded, 0);
+		add_url_param(url_buf, "compact", 1, 0);
+		add_url_param(url_buf, "event", event, 1);
+		
+		curl_easy_setopt(curl, CURLOPT_URL, url_buf);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, tracker_response_func);
+		res = curl_easy_perform(curl); 
+		if (res != CURLE_OK) {
+			fprintf(stderr, "curl_easy_perform() failed: %s\n",
+		    curl_easy_strerror(res));
+		}
+
+		curl_easy_cleanup(curl);
+	}
+	curl_global_cleanup();
+}
+
+
 int main(int argc, char *argv[])
 {
-		CURL *curl;
-	  CURLcode res;
-	 
-	  curl_global_init(CURL_GLOBAL_DEFAULT);
-	 
-	  curl = curl_easy_init();
-
-
-
-
 	int i;
 
 	setbuf(stdout, NULL);
