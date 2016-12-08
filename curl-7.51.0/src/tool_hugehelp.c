1c5f6628cf83b71e37d87f5e5130ce458dad743e
efriesne <emma_friesner@brown.edu>
2016-12-08 15:16:26 -0500

    added libcurl library

--- curl-7.51.0/src/tool_hugehelp.c
+++ curl-7.51.0/src/tool_hugehelp.c
@@ -2,7 +2,7 @@
 #ifndef HAVE_LIBZ
 /*
  * NEVER EVER edit this manually, fix the mkhelp.pl script instead!
- * Generation time: Wed Nov  2 07:33:11 2016
+ * Generation time: Wed Dec  7 21:19:49 2016
  */
 #ifdef USE_MANUAL
 #include "tool_hugehelp.h"
@@ -4586,7 +4586,7 @@ void hugehelp(void) {}
 #else
 /*
  * NEVER EVER edit this manually, fix the mkhelp.pl script instead!
- * Generation time: Wed Nov  2 07:33:12 2016
+ * Generation time: Wed Dec  7 21:19:50 2016
  */
 #ifdef USE_MANUAL
 #include "tool_hugehelp.h"
